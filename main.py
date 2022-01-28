0f88e0a28b93c4f1dc695994ceabf9caa6198b2b
kyletrent <ktjimmy99@gmail.com>
2022-01-28 17:09:35 -0500

    intial setup and creation

---
+++ main.py
@@ -0,0 +1,22 @@
+"""This is a test project for the use of vulneriblity scanning technology to help and aid the practice of security hardening"""
+import os
+import stat
+import datetime
+import urllib3
+import http
+import json
+import yaml
+
+
+
+
+class scan():
+
+    def __init__(self):
+        """"""
+
+
+
+if __name__ == '__main__':
+    """"""
+    print("Running main engine component")
